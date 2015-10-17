b7efd0a65c5a46254282dadd8d96e13eeee10505
ankit <ankit.kapoor83@gmail.com>
2015-10-17 00:19:30 -0400

    commit to master

--- something/src/something/home.java
+++ something/src/something/home.java
@@ -4,7 +4,11 @@ public class home {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< Upstream, based on origin/master_1
 		System.out.println("this is change 1.");
+=======
+		System.out.println("this is commit 3.");
+>>>>>>> d251514 commit to master
 
 	}
 
