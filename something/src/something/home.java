0e9d3018fcacfca29b6680812aaf86dfbbe7d5a3
ankitkapoor <ankitkapoor@192.168.1.2>
2015-10-17 23:40:59 -0400

    commit after conflict merging

--- something/src/something/home.java
+++ something/src/something/home.java
@@ -14,6 +14,7 @@ public class home {
 		System.out.println("commit to master 9");
 		System.out.println("commit to master 9");
 		System.out.println("something_1");
+		System.out.println("something new");
 	}
 
 }
